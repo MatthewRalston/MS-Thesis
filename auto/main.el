d04c05b21e258fafc55d86aaef7bd456b5cf7d96
MatthewRalston <mrals89@gmail.com>
2014-07-24 08:54:13 -0400

    minor update to methods, main

---
+++ auto/main.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook "main"
+ (lambda ()
+    (TeX-run-style-hooks
+     "hyperref"
+     "flexisym"
+     "subscript"
+     "siunitx"
+     "algorithm"
+     "algorithmic"
+     "cite"
+     "graphicx"
+     ""
+     "thesis-tap"
+     "methods"
+     "results"
+     "discussion"
+     "conclusions"
+     "bibtex")))
+
---
+++ auto/thesis-tap.el
@@ -0,0 +1,5 @@
+(TeX-add-style-hook "thesis-tap"
+ (lambda ()
+    (TeX-run-style-hooks
+     "acknowl")))
+
